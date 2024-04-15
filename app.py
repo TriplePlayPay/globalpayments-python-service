d700e4b13b0172a10def8b61a25112e1d16d14fd
DevTeam <noreply@tripleplaypay.com>
2024-04-15 17:20:10 -0400

    health checks

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from service import app
 
 if __name__ == '__main__':
+    """
     app.prepare(
         fast=True,
         motd=False,
         verbosity=0,
         access_log=False,
     )
+    """
     app.run(host="0.0.0.0")
--- service/__init__.py
+++ service/__init__.py
@@ -5,3 +5,6 @@ from service.blue_print import bp as bp_bp
 app = Sanic("GlobalPaymentsService")
 
 app.blueprint(bp_bp)
+
+app.config.HEALTH = True
+app.config.HEALTH_ENDPOINT = True
